72409bb5e51b9593c7c3887343182d23ced67090
sho55 <101057902+sho-itsuka@users.noreply.github.com>
2022-06-12 16:35:40 +0900

    Update Factorial.scala

--- Factorial.scala
+++ Factorial.scala
@@ -7,6 +7,7 @@ object Factorial extends App {
 }
 
 
+
 /* 学習メモ
 
 1 | import scala.math.BigInt
@@ -17,7 +18,7 @@ object Factorial extends App {
 
 3 | object Factorial extends App {}
   *オブジェクト Factorial の宣言。
-*
+
 4 | def factorial(i: BigInt): BigInt = if (i == 0)1 else i * factorial(i - 1)
   *階乗を表現するための関数。
   *関数宣言は、def という宣言ではじめ、ここでは factorial という名前の関数名を定義している。
@@ -30,4 +31,35 @@ object Factorial extends App {
   *4行目の式の意味は、「 i が 0 である場合には 1 を値をして返し、そうでない場合には i かける i-1 の階乗である」と定義されている。
   *factorial は再帰関数として宣言されており、 0 の階乗は 1 、それ以外の整数i の場合には、i かける i-1 の階乗であると定義されている。
 
+6 | println(factorial(1000))
+  *以上の実装は、1000の階乗の値をコンソールに出力して改行するものとなっている。
+
+■ まとめ
+  *Scala の数値は、整数と浮動小数点数で異なるデータの持ち方をする。
+  *Scala では、整数同士の割り算の結果では、小数点以下の値は無視されてしまう。
+  *非常に大きな整数を扱う際には、BigInt を利用する。
+
   */
+
+
+
+/* 問題
+  1, 129512円(税抜)のPCを消費税10%で購入した時の、税込の金額はいくらか(円未満切り捨て)。
+    scala> (129512 * 1.1).toInt
+    res0: Int = 142463
+
+  2, スタックオーバーフローが発生しないようにコードを修正する
+      *スタックオーバーフロー：再帰呼び出しには上限があり、その上限を超えたために起こってしまう例外のこと(保持するべき値が多くなりすぎてコンピューターが覚えきれなくなるために起こる)。
+
+      import scala.math.BigInt
+
+      object FactorialTailRec extends App {
+        def factorial(i: BigInt, acc: BigInt): BigInt = if (i == 0) acc else (i - 1, i * acc)
+        println(factorial(30000,1))
+      }
+    
+    再帰関数の戻り値が必ず【再帰関数の呼び出し】 or 【値】 となるように再帰関数を実装すると、内部的にループの表現に変換される末尾再帰最適化が行われる。
+    ここでは、acc(accumulator(アキュムレーター)の略、貯める・蓄積する) という引数を定義し、末尾再帰最適化が起こるように修正してある。
+
+*/
+  
