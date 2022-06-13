d15e21ab03ffcc10e37918be360f5e30568f8553
sho55 <101057902+sho-itsuka@users.noreply.github.com>
2022-06-14 00:20:32 +0900

    Update Factorization.scala

--- Factorization.scala
+++ Factorization.scala
@@ -19,3 +19,12 @@ object Factorization extends App {
 /* 解説
  1 | import scala.math.sqrt
     平方根を求める関数 def sqrt(x:Double): Double を利用できるようにしている。
+
+ 5 | val maxDivisor = sqrt(target).toInt
+    sqrt 関数を利用して「割る数の最大値」を計算している。
+    ただし、sqrt 関数は戻り値の型が浮動小数点数であるDouble値であるため、toInt メソッドを呼ぶことでInt型に変換している。
+    これで小数点部分は切る捨てられる。
+    最後に println 関数で「悪数の最大値」をコンソールに出力してある。
+
+
+ */
