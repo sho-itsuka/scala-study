2a2ac2b844648cb6cb8b300704b146d8e0e89c70
sho55 <101057902+sho-itsuka@users.noreply.github.com>
2022-06-18 18:05:03 +0900

    Update PracticeFactorization.scala

--- PracticeFactorization.scala
+++ PracticeFactorization.scala
@@ -1,6 +1,7 @@
+// Factorization の練習問題(24の素因数分解の結果を取得)
 import scala.math.sqrt
 
-object Factorization extends App {
+object PracticeFactorization extends App {
 
   def factorization(target: Int): Map[Int, Int] = {
     val maxDivisor = sqrt(target).toInt
@@ -9,7 +10,7 @@ object Factorization extends App {
       if (divisor > maxDivisor) {
         if (num == 1) acc else acc + (num -> 1)
       } else if (num % divisor == 0) {
-        val next acc + (divisor -> (acc.getOrElse(divisor, 0) + 1))
+        val nextAcc = acc + (divisor -> (acc.getOrElse(divisor, 0) + 1))
         factorizationRec(num / divisor, divisor, nextAcc)
       } else {
         factorizationRec(num, divisor + 1, acc)
