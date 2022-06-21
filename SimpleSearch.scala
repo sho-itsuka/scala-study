44ac814afc0b6f686f9851273182f29743f89eca
sho55 <101057902+sho-itsuka@users.noreply.github.com>
2022-06-22 01:19:04 +0900

    Update SimpleSearch.scala

--- SimpleSearch.scala
+++ SimpleSearch.scala
@@ -82,4 +82,12 @@
 
   */
 
+
+object SimpleSearch extends App {
+  val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
+  val pattern = "ドワンゴ".toSeq
+  val matchIndexes = Seq()
+
+  println(s"出現場所: ${matchIndexes}")
+}
  
