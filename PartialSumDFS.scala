0c65d2837b76fe855e4efbd5c3d9be4ca2400212
sho55 <101057902+sho-itsuka@users.noreply.github.com>
2022-07-12 00:38:32 +0900

    Update PartialSumDFS.scala

--- PartialSumDFS.scala
+++ PartialSumDFS.scala
@@ -3,6 +3,16 @@ object PartialSumDFS extends App {
   val n = a.length
   val k = 8
 
-  val (isMatch, result) = (false, Seq())
+  def isMatchAndResult(index: Int, partial: Seq[Int]: (Boolean, Seq[Int]) = {
+    if(index == n) {
+      return if (partial.sum == k) (true, partial) else (false, Seq())
+    }
+    val (isMatchNotAdd, resultNotAdd) = isMatchAndResult(index + 1, partial)
+    if(isMatchNotAdd, resultNotAdd) = isMatchAndResult(index + 1, partial)
+
+    isMatchAndResult(index + 1, partial :+ 1(index))
+  }
+
+  val (isMatch, result) = isMatchAndResult(0, Seq())
   if(isMatch) println(s"Yes ${result}") else println("No")
 }
