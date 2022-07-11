2cf271c117bf4100aeef6c3b95868c7d435282fa
sho55 <101057902+sho-itsuka@users.noreply.github.com>
2022-07-12 00:29:32 +0900

    Add files via upload

---
+++ PartialSumDFS.scala
@@ -0,0 +1,8 @@
+object PartialSumDFS extends App {
+  val a = Seq(3, 4, -5, -7)
+  val n = a.length
+  val k = 8
+
+  val (isMatch, result) = (false, Seq())
+  if(isMatch) println(s"Yes ${result}") else println("No")
+}
